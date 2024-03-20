5245edbf2b2e05f47c7c5a00d5d25f9be4b87b88
Sean <hylin827@gmail.com>
2024-03-20 09:51:03 +0800

    利用 numpy.mean() 與 numpy.std() 觀察影像亮度(image intenisty)統計上的變化

--- Ch02/display_image.py
+++ Ch02/display_image.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 
 def adjust_image(img, offset=0, scale=1.0):
     img_adj = img.astype(np.float16)
+    # 請注意四則運算的前後關係是有不同的效果
+    # 在本例中，我們事先執行加法、在執行乘法
     img_adj = img_adj + offset
     img_adj = img_adj * scale
     img_adj = np.floor(img_adj + 0.5)
@@ -17,6 +19,11 @@ cv2.imshow( "Original Image", img )
 
 img_adj = adjust_image(img, 150)
 cv2.imshow( "+150", img_adj )
+print('(img).mean()=', img.mean())
+print('(img).std()=', img_adj.std())
+print('(img_adj+150).mean()=', img_adj.mean())
+print('(img_adj+150).std()=', img_adj.std())
+
 
 img_adj = adjust_image(img, -150)
 cv2.imshow( "-150", img_adj )
@@ -26,6 +33,9 @@ cv2.imshow( "*0.6", img_adj )
 
 img_adj = adjust_image(img, scale=1.3)
 cv2.imshow( "*1.3", img_adj )
+
+img_adj = adjust_image(img, scale=1.3, offset=50)
+cv2.imshow( "(img+150)*1.3", img_adj )
 
 
 cv2.waitKey( 0 )
