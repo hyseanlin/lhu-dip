c63457d245a2dcb29a3141c6bbc9db10d11df317
Sean <hylin827@gmail.com>
2024-03-20 11:31:07 +0800

    add function for displaying image histogram

--- Ch02/display_image.py
+++ Ch02/display_image.py
@@ -18,10 +18,24 @@ def convert_image_to_unit8(img):
     img_adj[img_adj < 0 ] = 0
     return img_adj.astype(np.uint8)
 
+def histogram( f ):
+	if f.ndim != 3:
+		hist = cv2.calcHist( [f], [0], None, [256], [0,256] )
+		plt.plot( hist )
+	else:
+		color = ( 'b', 'g', 'r' )
+		for i, col in enumerate( color ):
+			hist = cv2.calcHist( f, [i], None, [256], [0,256] )
+			plt.plot( hist, color = col )
+	plt.xlim( [0,256] )
+	plt.xlabel( "Intensity" )
+	plt.ylabel( "#Intensities" )
+	plt.show( )
+    
 img = cv2.imread( "..\dataset\Lena.pgm", -1 )
 cv2.imshow( "Original Image", img.astype(np.uint8) )
 print(f'In the original image, mean={img.mean()}, std={img.std()}')
-
+histogram(img)
 
 img_adj = adjust_image_by_offset(img, 150)
 # cv2.imshow( "+150", img_adj)
@@ -50,8 +64,10 @@ print(f'In the adjusted image, mean={img_adj.mean()}, std={img_adj.std()}')
 img_adj = adjust_image_by_scaling(img_adj, 0.5)
 print(f'In the adjusted image, mean={img_adj.mean()}, std={img_adj.std()}')
 img_adj = adjust_image_by_offset(img_adj , img.mean())
-cv2.imshow( "make standard deviation smaller", img_adj.astype(np.uint8) )
+img_adj =  convert_image_to_unit8(img_adj)
+cv2.imshow( "make standard deviation smaller", img_adj  )
 print(f'In the adjusted image, mean={img_adj.astype(np.uint8).mean()}, std={img_adj.astype(np.uint8).std()}')
+histogram(img_adj)
 
 
 img_adj = adjust_image_by_offset(img, -img.mean())
@@ -59,16 +75,20 @@ print(f'In the adjusted image, mean={img_adj.mean()}, std={img_adj.std()}')
 img_adj = adjust_image_by_scaling(img_adj, 1.1)
 print(f'In the adjusted image, mean={img_adj.mean()}, std={img_adj.std()}')
 img_adj = adjust_image_by_offset(img_adj , img.mean())
-cv2.imshow( "make standard deviation larger 1", img_adj.astype(np.uint8) )
+img_adj =  convert_image_to_unit8(img_adj)
+cv2.imshow( "make standard deviation larger 1", img_adj  )
 print(f'In the adjusted image, mean={img_adj.astype(np.uint8).mean()}, std={img_adj.astype(np.uint8).std()}')
+histogram(img_adj)
 
 img_adj = adjust_image_by_offset(img, -img.mean())
 print(f'In the adjusted image, mean={img_adj.mean()}, std={img_adj.std()}')
 img_adj = adjust_image_by_scaling(img_adj, 1.3)
 print(f'In the adjusted image, mean={img_adj.mean()}, std={img_adj.std()}')
 img_adj = adjust_image_by_offset(img_adj , img.mean())
-cv2.imshow( "make standard deviation larger 2", convert_image_to_unit8(img_adj) )
+img_adj = convert_image_to_unit8(img_adj)
+cv2.imshow( "make standard deviation larger 2", img_adj  )
 print(f'In the adjusted image, mean={img_adj.astype(np.uint8).mean()}, std={img_adj.astype(np.uint8).std()}')
+histogram(img_adj)
 
 
 
