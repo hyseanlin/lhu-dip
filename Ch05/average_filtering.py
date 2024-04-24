c7a48c2207023d909f4bf46c1feca21c0267eb07
Sean Lin <hylin827@gmail.com>
2024-04-24 11:43:35 +0800

    updated: demo code for filter usages

--- Ch05/average_filtering.py
+++ Ch05/average_filtering.py
@@ -1,8 +1,18 @@
 import numpy as np
 import cv2
+from scipy.signal import convolve2d
 
-img1 = cv2.imread( "Lenna.bmp", -1 )
-img2 = cv2.blur( img1, ( 5, 5 ) )
+img1 = cv2.imread( "../dataset/lena.pgm", 0)
+img2 = cv2.blur( img1, ( 3, 3 ) )
+'''
+our_filter = np.array( [[0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 1] ] )
+img2 = convolve2d( img1, our_filter, 'same' )
+img2 = img2.astype(np.uint8)
+'''
 cv2.imshow( "Original Image", img1 )	
 cv2.imshow( "Average Filtering", img2 )
 cv2.waitKey( 0 )
--- Ch05/gaussian_filtering.py
+++ Ch05/gaussian_filtering.py
@@ -1,8 +1,11 @@
 import numpy as np
 import cv2
+filter_size = 5
+sigma = 0
+img1 = cv2.imread( "../dataset/lena.pgm", 0)
+img2 = cv2.GaussianBlur( img1, ( filter_size , filter_size  ), 0 )
+# cv2.imshow( "Original Image", img1 )	
+# cv2.imshow( "Gaussian Filtering", img2 )
+cv2.imwrite(f'lenna_gauss_filtered_sz{filter_size}_sigma{sigma}.png', img2)
 
-img1 = cv2.imread( "Lenna.bmp", -1 )
-img2 = cv2.GaussianBlur( img1, ( 5, 5 ), 0 )
-cv2.imshow( "Original Image", img1 )	
-cv2.imshow( "Gaussian Filtering", img2 )
 cv2.waitKey( 0 )
