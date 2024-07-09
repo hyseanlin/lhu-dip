0600b0eb37a3a273033d2d1f880735ab7798c2ec
Sean Lin <hylin827@gmail.com>
2024-07-09 11:25:22 +0800

    no message

--- Ch03/image_quantization.py
+++ Ch03/image_quantization.py
@@ -10,7 +10,7 @@ def image_quantization( f, bits ):
 	table = np.zeros( 256 )
 	for k in range( 256 ):
 		for l in range( levels ):
-			if k >= l * interval and l < ( l + 1 ) * interval:
+			if k >= l * interval and k < ( l + 1 ) * interval:
 				table[k] = round( l * gray_level_interval ) 
     
 	for x in range( nr ):
@@ -20,10 +20,10 @@ def image_quantization( f, bits ):
 	return g
 	
 def main( ):
-	img1 = cv2.imread( "../dataset/lena.ppm", -1 )
-	img2 = image_quantization( img1, 2 )
-	img3 = image_quantization( img1, 3 )
-	img4 = image_quantization( img1, 2 )
+	img1 = cv2.imread( "barbara.bmp", -1 )
+	img2 = image_quantization( img1, 7 )
+	img3 = image_quantization( img1, 5 )
+	img4 = image_quantization( img1, 3 )
 	img5 = image_quantization( img1, 1 )
 	cv2.imshow( "Original Image", img1 )
 	cv2.imshow( "Quantization (5-bit)", img2 )
